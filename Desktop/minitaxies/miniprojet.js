0cb2ef1f0b903c11d9d834c6a81deb4e3ff8050a
ennafiaasma-art <ennafiaasma@gmail.com>
2025-11-03 14:37:11 +0100

    Refactor taxi management logic and variable names
    Refactor taxi management code for better readability and organization.

--- Desktop/minitaxies/miniprojet.js
+++ Desktop/minitaxies/miniprojet.js
@@ -1,77 +1,79 @@
 
-const prompt=require("prompt-sync")();
+const prompt = require("prompt-sync")();
 
-let taxis =[
-{id:1 , position:4,desponible:true ,tempstrajet:0 ,totalrides:0 },
-{id:5 , position:6,desponible:true ,tempstrajet:0 ,totalrides:0},
-{id:9 , position:13,desponible:true ,tempstrajet:0 ,totalrides:0}
+let taxis = [
+    { id: 1, position: 4, desponible: true, tempstrajet: 0, totalrides: 0 },
+    { id: 5, position: 6, desponible: true, tempstrajet: 0, totalrides: 0 },
+    { id: 9, position: 13, desponible: true, tempstrajet: 0, totalrides: 0 }
 ];
-let requests = [
-{ reqId: 1, position: 1, duration: 3, time: 0 },
-{ reqId: 2, position: 5, duration: 4, time: 2 },
-{ reqId: 8, position: 12, duration: 2, time: 4 },
-{ reqId: 4, position: 7, duration: 5, time: 5 }
-];
+// let Request = [
+//     { reqId: 1, position: 1, duration: 3, time: 0 },
+//     { reqId: 2, position: 5, duration: 4, time: 2 },
+//     { reqId: 3, position: 12, duration: 2, time: 4 },
+//     { reqId: 4, position: 7, duration: 5, time: 5 }
+// ];
+let Request=[];
 
 
+let tab = [];
+function disponible() {
 
-let tab=[];
-function disponible(){
+    for (let i = 0; i < taxis.length; i++) {
+        if (taxis[i].desponible == true) {
+            tab.push(taxis[i])
+            console.log("taxies disponible : " + taxis[i].id);
 
-for(let i =0;i<taxis.length;i++){
-if (taxis[i].desponible==true){
-tab.push(taxis[i])
-console.log("taxies disponible : "+ taxis[i].id);
-
-}
-
-
-}
-
-}
+        }
 
 
-// disponible()
-
-
-
-function roturn(){
-let pos=Number(prompt("entrer votre position :  "));
-let min = Infinity;
-let id ;
-for(let i=0;i<tab.length;i++){
-let n=tab[i].position;
-let compar = Math.abs(n-pos);
-if(min>compar){
-min=compar
-id=tab[i]
-}
+    }
 
 }
-console.log(id);
+
+
+
+
+
+function rechercheTaxiDisponibleetproche() {
+    let pos = Number(prompt("entrer votre position :  "));
+    let min = +Infinity;
+    let id;
+    for (let i = 0; i < tab.length; i++) {
+        let compar = Math.abs(tab[i].position - pos);
+        if (min > compar) {
+            min = compar
+            id = tab[i]
+        }
+
+
+    }
+    console.log(id);
 
 
 }
 disponible()
-roturn()
+rechercheTaxiDisponibleetproche()
 
+function AjouterDemande() {
+    let comptoirRequest = Request.length
+    console.log("Ajouter une demande :");
+    let n = parseInt(prompt(" combien de Demandes voulez-vous ajouter :"))
+    for (let i = 0; i < n; i++) {
+        console.log(`\n Demande N° : ${i + 1} `);
+        let reqId = comptoirRequest + 1
+        let position = parseInt(prompt("entrez la position :"))
+        let duration = parseInt(prompt("Entrez la durée :"))
+        
+        Request.push({ reqId, position, duration})
+    }
+    console.log("\n La demande :");
+    console.log(Request)
+}
+AjouterDemande()
 
+function durationDetrajet(taxis) {
 
-
-
-function duration(){
-
-const debutheur =Number(prompt("entrer heur   de depart :  "));
-let finheur=Number(prompt("entrer tepms final de trajet :"))
-let n=taxis[i].tempstrajet
-for(let i=0;taxis.length;i++){
-if(n==debutheur){
-    console.log(taxis[i].id+"occupée ");
-    
-}else if (n==finheur){
-return disponible()
 }
 
-}
-}
-duration()
+
+
